46501e9ae1882878e53c21934585b503249cafbd
{104104104} <{rz344788@yahoo.co.jp}>
2020-12-19 19:35:35 +0900

    ファイル名を調整

--- list.js
+++ list.js
@@ -4,43 +4,47 @@ const BASE_LIST_URL = 'http://0.0.0.0:8402';
 
 var NAME_LIST = [];
 
-let promise = new Promise((resolve, reject) => {
-    console.log('startPromise')
-    resolve()
-})
 
-promise.then(() => {
-    return new Promise((resolve, reject) => {
-        var getdata;
-        url = BASE_LIST_URL + '/get';
+//読み込み時、ワークスペースのリストを表示
+window.onload = (event) => {
+    let promise = new Promise((resolve, reject) => {
+        console.log('startPromise')
+        resolve()
+    })
 
-        let url_obj = new URL(url);
-        fetch(url_obj.toString(), {
-                method: 'GET',
-            }, )
-            .then(function(response) {
-                return response.text();
-            })
-            .then(function(text) {
-                getdata = JSON.parse(JSON.parse(text));
-                NAME_LIST = getdata;
-                resolve(getdata);
-            });
-    });
-}).then((data) => {
-    console.log(data)
-    for (let name of data) {
-        console.log(name)
-        var newElement = document.createElement("a");
-        var newContent = document.createTextNode('表示名:' + name.displayName + ' id:' + name.datafileID);
-        newElement.appendChild(newContent);
+    promise.then(() => {
+        return new Promise((resolve, reject) => {
+            var getdata;
+            url = BASE_LIST_URL + '/get';
 
-        newElement.href = './index.html?filenameID=' + name.datafileID;
+            let url_obj = new URL(url);
+            fetch(url_obj.toString(), {
+                    method: 'GET',
+                }, )
+                .then(function(response) {
+                    return response.text();
+                })
+                .then(function(text) {
+                    getdata = JSON.parse(JSON.parse(text));
+                    NAME_LIST = getdata;
+                    resolve(getdata);
+                });
+        });
+    }).then((data) => {
+        console.log(data)
+        for (let name of data) {
+            console.log(name)
+            var newElement = document.createElement("a");
+            var newContent = document.createTextNode('表示名:' + name.displayName + ' id:' + name.datafileID);
+            newElement.appendChild(newContent);
 
-        newElement.style.display = 'block';
+            newElement.href = './main.html?filenameID=' + name.datafileID;
 
-        body.appendChild(newElement);
-    }
-}).catch(() => { // エラーハンドリング
-    console.error('Something wrong!')
-})
+            newElement.style.display = 'block';
+
+            body.appendChild(newElement);
+        }
+    }).catch(() => { // エラーハンドリング
+        console.error('Something wrong!')
+    })
+}
